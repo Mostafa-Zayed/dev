fbc521c4adaaae1625dad73e9e74baa7d91a6363
Mostafa Zayed <mostafa.zayed9990@gmail.com>
2024-03-04 20:09:14 +0200

    testing

---
+++ Modules/Website/Resources/views/includes/feature_template1.blade.php
@@ -0,0 +1,49 @@
+<section id="AppFeatures" class="section-block features-style2" data-scroll-index="1">
+    <div class="container">
+        <div class="section-header">
+            @if(! empty($settings))
+            <h2>{{($settings->getTranslations('section_features_title')[app()->getLocale()])}}</h2>
+            {!! $settings->getTranslations('section_features_description')[app()->getLocale()] !!}
+            @endif
+        </div>
+        <div class="row">
+            @if(! empty($features) && $features->count() > 0)
+            <div class="col-md-4">
+                @foreach ($features as $feature)
+                <div class="feature-block">
+                    <span class="feature-icon">
+                        <img src="images/icons/1.jpg" class="img-fluid" alt="Img" />
+                    </span>
+                    <div class="feature-content">
+                        <h3>Trendy Look</h3>
+                        <p>We provide marketing to businesses digital media.</p>
+                    </div>
+                </div>
+                array_shift($features)
+                @endforeach
+                <div class="col-md-4">
+                    <div class="img-box">
+                        <img src="{{$settings->section_features_image}}" class="img-fluid" alt="Img" />
+                    </div>
+                </div>
+                @foreach ($features as $feature)
+                <div class="feature-block">
+                    <span class="feature-icon">
+                        @if($feature->icon)
+                        <i class="{{$feature->icon}}"></i>
+                        @else
+                        <img src="{{$feature->image}}" class="img-fluid" alt="Img" />
+                        @endif
+                    </span>
+                    <div class="feature-content">
+                        <h3>{{$feature->getTranslations('name')[app()->getLocale()]}}</h3>
+                        {!! $feature->getTranslations('description')[app()->getLocale()] !!}
+                    </div>
+                </div>
+                array_shift($features)
+                @endforeach
+            </div>
+            @endif
+        </div>
+    </div>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -146,53 +146,7 @@
     <!-- End Home Area -->
 
     <!-- Start Features Section -->
-    <section id="AppFeatures" class="section-block features-style1" data-scroll-index="1">
-        @if(empty($settings->section_features_image))
-        <div class="circls-features active">
-            <div class="circle-1"></div>
-            <div class="circle-2"></div>
-            <div class="circle-3"></div>
-            <div class="circle-4"></div>
-            <div class="circle-x"></div>
-        </div>
-        @endif
-        <div class="container">
-            <div class="section-header">
-                @if(! empty($settings))
-                <h2>{{($settings->getTranslations('section_features_title')[app()->getLocale()])}}</h2>
-                {!! $settings->getTranslations('section_features_description')[app()->getLocale()] !!}
-                @endif
-            </div>
-            <div class="row">
-                <!-- Start Features Item -->
-                @if(! empty($features) && $features->count() > 0)
-                @foreach ($features as $feature)
-                @if ($loop->index == 3 && ! empty($settings->section_features_image))
-                <div class="col-md-4">
-                        <div class="img-box">
-                            <img src="{{$settings->section_features_image}}" class="img-fluid" alt="Img" />
-                        </div>
-                    </div>
-                @endif
-                <div class="col-md-4">
-                    <div class="feature-block">
-                        <span class="feature-icon icon-1">
-                            @if($feature->icon)
-                            <i class="{{$feature->icon}}"></i>
-                            @else
-                            <img src="{{$feature->image}}" class="img-fluid" alt="Img" />
-                            @endif
-                        </span>
-                        <h3>{{$feature->getTranslations('name')[app()->getLocale()]}}</h3>
-                        {!! $feature->getTranslations('description')[app()->getLocale()] !!}
-                    </div>
-                </div>
-                @endforeach
-                <!-- End Features Item -->
-                @endif
-            </div>
-        </div>
-    </section>
+    @include(Website::includs.feature_template1)
     <!-- End Features Section -->
 
     <!-- Start How it works Section -->
