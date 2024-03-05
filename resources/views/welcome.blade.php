30244ffa0a1e107e00b7c7c41135482bef925966
Mostafa Zayed <mostafa.zayed9990@gmail.com>
2024-03-05 20:28:57 +0200

    add slider template1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -99,50 +99,9 @@
     <!-- End Header Area-->
 
     <!-- Start Home Area -->
-    <section class="start_home demo1">
-        <div class="banner_top">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-6 start-home-content">
-                        <h1>
-                            {{$template->websiteSlider->heading}}
-                        </h1>
-                        {!! $template->websiteSlider->description !!}
-                        <div class="app-button">
-                            <div class="apple-button">
-                                <a href="{{$template->websiteSlider->app_store_link}}">
-                                    <div class="slider-button-icon">
-                                        <i class="fab fa-apple" aria-hidden="true"></i>
-                                    </div>
-                                    <div class="slider-button-title">
-                                        <p>from App store</p>
-                                        <h3>app store</h3>
-                                    </div>
-                                </a>
-                            </div>
-                            <div class="google-button">
-                                <a href="{{$template->websiteSlider->google_play_link}}">
-                                    <div class="slider-button-icon">
-                                        <i class="fab fa-google-play" aria-hidden="true"></i>
-                                    </div>
-                                    <div class="slider-button-title">
-                                        <p>From Play Store</p>
-                                        <h3>Google Play</h3>
-                                    </div>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 start-home-img">
-                        <img class="img-fluid" src="{{asset($template->websiteSlider->image)}}" alt="Img" />
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="shape-bottom">
-            <img src="images/shapes/shape-1.svg" alt="shape" class="bottom-shape img-fluid">
-        </div>
-    </section>
+    @if($template->number == 1)
+        @include('website::includes.slider_template1')
+    @endif
     <!-- End Home Area -->
 
     <!-- Start Features Section -->
