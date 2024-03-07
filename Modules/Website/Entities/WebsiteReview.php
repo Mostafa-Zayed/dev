c3aefa7c68449377d4189cc63db833be56693607
Mostafa Zayed <mostafa.zayed9990@gmail.com>
2024-03-07 19:30:25 +0200

    add reviews

--- Modules/Website/Entities/WebsiteReview.php
+++ Modules/Website/Entities/WebsiteReview.php
@@ -31,6 +31,6 @@ class WebsiteReview extends Model
 
     public function user()
     {
-        return $this->belongsTo(App\User::class,'user_id','id');
+        return $this->belongsTo('App\User','user_id','id');
     }
 }
