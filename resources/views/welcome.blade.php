1691d6e7c9fa28a02e1346c4addac57a6b265dc5
Mostafa Zayed <mostafa.zayed9990@gmail.com>
2024-03-13 21:31:30 +0200

    update navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -54,24 +54,10 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarCollapse">
                     <ul class="navbar-nav">
-                        <li class="nav-item dropdown">
+                        <li class="nav-item">
                             <a class="dropdown-toggle" href="#" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{{__('lang_v1.home')}}</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="dropdown-toggle" href="#" id="dropdownMenuButton2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Pages</a>
-                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                                <a class="dropdown-item" href="about-us.html">About Us</a>
-                                <a class="dropdown-item" href="contact-us.html">Contact Us</a>
-                                <a class="dropdown-item" href="faqs.html">Faqs</a>
-                                <a class="dropdown-item" href="reviews.html">Reviews</a>
-                                <a class="dropdown-item" href="login.html">Login</a>
-                                <a class="dropdown-item" href="signup.html">Signup</a>
-                                <a class="dropdown-item" href="forget-password.html">Forget Password</a>
-                                <a class="dropdown-item" href="reset-password.html">Reset Password</a>
-                                <a class="dropdown-item" href="coming-soon.html">Coming Soon</a>
-                                <a class="dropdown-item" href="page-404.html">Page 404</a>
-                            </div>
-                        </li>
+                      
                         <li class="nav-item"><a href="#" data-scroll-nav="1">{{__('lang_v1.features')}}</a></li>
                         <li class="nav-item"><a href="#" data-scroll-nav="2">{{__('lang_v1.how_work')}}</a></li>
                         <li class="nav-item"><a href="#" data-scroll-nav="3">Screenshots</a></li>
@@ -79,6 +65,8 @@
                         <li class="nav-item"><a href="#" data-scroll-nav="7">Reviews</a></li>
                         <li class="nav-item"><a href="#" data-scroll-nav="6">Faqs</a></li>
                         <li class="nav-item"><a href="#" data-scroll-nav="8">Contact</a></li>
+                        <li class="nav-itme"><a href="{{route('login')}}">{{__('business.login')}}</a></li>
+                        <li class="nav-itme"><a href="{{route('getRegister')}}">{{__('business.register')}}</a></li>
                     </ul>
 
                 </div>
