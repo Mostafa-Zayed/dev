692ffeb6bb536cb3a27ed6eecfec12ba55fa0e4e
Mostafa Zayed <mostafa.zayed9990@gmail.com>
2024-03-21 16:06:55 +0200

    website pages

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -11,8 +11,24 @@
     <link rel="icon" href="{{asset('favicon.png')}}" type="image/gif" sizes="32x32">
     <meta name="keywords" content="ERP TEC" />
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
+    @if(getLanguage() == 'ar')
     <!-- Font Icons -->
-    <link rel="stylesheet" href="{{asset('website/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('website/rtl/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('website/rtl/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('website/rtl/css/flaticon.css')}}">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/bootstrap.min.css')}}">
+    <!-- Animation -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/animate.min.css')}}">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/owl.carousel.min.css')}}">
+    <!-- Light Case -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/lightcase.min.css')}}" type="text/css">
+    <!-- Template style -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/style.css')}}">
+    @else
+     <!-- Font Icons -->
+     <link rel="stylesheet" href="{{asset('website/css/fontawesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('website/css/themify-icons.css')}}">
     <link rel="stylesheet" href="{{asset('website/css/flaticon.css')}}">
     <!-- Bootstrap -->
@@ -25,7 +41,7 @@
     <link rel="stylesheet" href="{{asset('website/css/lightcase.min.css')}}" type="text/css">
     <!-- Template style -->
     <link rel="stylesheet" href="{{asset('website/css/style.css')}}">
-
+    @endif
     <link rel="stylesheet" type="text/css" href="{{asset('css/toastr.css')}}">
     <link rel="stylesheet" type="text/css" href="{{asset('css/rtl/toastr.min.css')}}">
     <!--[if lt IE 9]>
--- resources/views/reviews.blade.php
+++ resources/views/reviews.blade.php
@@ -11,8 +11,24 @@
     <link rel="icon" href="{{asset('favicon.png')}}" type="image/gif" sizes="32x32">
     <meta name="keywords" content="ERP TEC" />
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;700;900&display=swap" rel="stylesheet">
+    @if(getLanguage() == 'ar')
     <!-- Font Icons -->
-    <link rel="stylesheet" href="{{asset('front/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('website/rtl/css/fontawesome.min.css')}}">
+    <link rel="stylesheet" href="{{asset('website/rtl/css/themify-icons.css')}}">
+    <link rel="stylesheet" href="{{asset('website/rtl/css/flaticon.css')}}">
+    <!-- Bootstrap -->website/rtl
+    <link rel="stylesheet" href="{{asset('website/rtl/css/bootstrap.min.css')}}">
+    <!-- Animation -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/animate.min.css')}}">
+    <!-- Owl Carousel -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/owl.carousel.min.css')}}">
+    <!-- Light Case -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/lightcase.min.css')}}" type="text/css">
+    <!-- Template style -->
+    <link rel="stylesheet" href="{{asset('website/rtl/css/style.css')}}">
+    @else
+     <!-- Font Icons -->
+     <link rel="stylesheet" href="{{asset('front/css/fontawesome.min.css')}}">
     <link rel="stylesheet" href="{{asset('front/css/themify-icons.css')}}">
     <link rel="stylesheet" href="{{asset('front/css/flaticon.css')}}">
     <!-- Bootstrap -->
@@ -25,6 +41,7 @@
     <link rel="stylesheet" href="{{asset('front/css/lightcase.min.css')}}" type="text/css">
     <!-- Template style -->
     <link rel="stylesheet" href="{{asset('front/css/style.css')}}">
+    @endif
     <!--[if lt IE 9]>
           <script src="js/html5shiv.min.js"></script>
           <script src="js/respond.min.js"></script>
