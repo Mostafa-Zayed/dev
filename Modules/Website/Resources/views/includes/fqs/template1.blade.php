73bf762b7ace95e5d028596dff6956d173fd5a9b
alaahamdi7 <ahmadsamerlego@gmail.com>
2024-04-02 23:28:52 +0200

    faqs-paragraph

--- Modules/Website/Resources/views/includes/fqs/template1.blade.php
+++ Modules/Website/Resources/views/includes/fqs/template1.blade.php
@@ -1,42 +1,49 @@
 <section id="faqs" class="section-block" data-scroll-index="6">
-        <div class="container">
-            <div class="section-header">
-                @if(! empty($settings))
+    <div class="container">
+        <div class="section-header">
+            @if (!empty($settings))
                 <h2>
-                    @if(! empty($settings->getTranslations('section_questions_title')[app()->getLocale()])) {{$settings->getTranslations('section_questions_title')[app()->getLocale()]}}@endif
+                    @if (!empty($settings->getTranslations('section_questions_title')[app()->getLocale()]))
+                        {{ $settings->getTranslations('section_questions_title')[app()->getLocale()] }}
+                    @endif
                 </h2>
-                @if(! empty($settings->getTranslations('section_questions_description')[app()->getLocale()])) {!! $settings->getTranslations('section_questions_description')[app()->getLocale()] !!} @endif
+                @if (!empty($settings->getTranslations('section_questions_description')[app()->getLocale()]))
+                    {!! $settings->getTranslations('section_questions_description')[app()->getLocale()] !!}
                 @endif
-            </div>
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="img-box">
-                        <img src="@if(! empty($settings->section_questions_image)) {{$settings->section_questions_image}} @endif" class="img-fluid" alt="Img" />
-                    </div>
+            @endif
+        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <div class="img-box">
+                    <img src="@if (!empty($settings->section_questions_image)) {{ $settings->section_questions_image }} @endif"
+                        class="img-fluid" alt="Img" />
                 </div>
-                <div class="col-md-6">
-                    <div class="accordion" id="accordionExample">
-                        @foreach ($questions as $question)
-                            <!-- Start Faq item -->
+            </div>
+            <div class="col-md-6">
+                <div class="accordion" id="accordionExample">
+                    @foreach ($questions as $question)
+                        <!-- Start Faq item -->
                         <div class="card">
                             <div class="card-header" id="headingOne">
                                 <h2 class="mb-0">
-                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-                                        {{$question->name}}
+                                    <button class="btn btn-link" type="button" data-toggle="collapse"
+                                        data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                        {{ $question->name }}
                                     </button>
                                 </h2>
                             </div>
 
-                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                data-parent="#accordionExample">
                                 <div class="card-body">
-                                    {{$question->answer}}
+                                    {!! $question->answer !!}
                                 </div>
                             </div>
                         </div>
                         <!-- End Faq item -->
-                        @endforeach
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
-    </section>
+    </div>
+</section>
